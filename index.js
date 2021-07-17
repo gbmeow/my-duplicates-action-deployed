7b512c1a8a3be5d7bac37f7062613b0325ff0a29
George Batalinski <george.batalinski@gmail.com>
2021-07-17 14:02:39 -0400

    add table generator for pull request comment

--- index.js
+++ index.js
@@ -1,23 +1,66 @@
 const github = require('@actions/github');
 const core = require('@actions/core');
+const stats = require('webpack-stats-duplicates');
 
-const octokit = github.getOctokit(`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`)
+const octokit = github.getOctokit(`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`);
+
+const statsLoad = async () => {
+    return new Promise((resolve, reject) => {
+        let duplicates; 
+        stats.loadConfig('./stats.json', (error, json) => {
+        if (error) {
+          console.log('Error');
+          return;
+        }
+        duplicates = stats.findDuplicates(json, {
+          whitelist: [ ]
+        });
+      });
+        resolve(duplicates);
+    });
+  
+  };
 
 myAsyncMethod();
 
+
+//show the duplicates to remove   
+
+
 async function myAsyncMethod () {
     try {
-        const { data: pullRequest } = await octokit.rest.issues.createComment({
+        const dups = await statsLoad();
+        var result = generateTable();
+        await octokit.rest.issues.createComment({
             owner: 'georgebatalinski',
             repo: 'questions',
             issue_number: 2,
-            body: 'your coverrage is off a little',
-          })
-    
-        console.log(pullRequest);
+            body: result,
+          });
     } catch(ex) {
-        console.log( ex );
+        console.log( 'Error', ex );
     }
-  
-  
 }
+
+function generateTable() {
+    let str = "<table>";
+    str += "<thead><tr>"
+    str += "<th>Name</th><th>Duplicate Location</th><th>Existing Location</th>";
+    str += "</tr></thead>";
+    str += "<tbody>"
+    str += "<tr>";
+    var cell = getDataCell('froala');
+    cell += getDataCell('./node_modules/css-loader/froala');
+    cell += getDataCell('./node_modules/css-loader/anotherpath/froala'); 
+    str += cell;
+    str += "</tr>"
+    str += "</tbody>"
+    str += "</table>";
+    return str;
+}
+
+function getDataCell(data) {
+    return "<td>" + data + "</td>";
+}
+
+
