8b1fc0526d74c100a4814ff6686a29b9ebc4e8be
George Batalinski <george.batalinski@gmail.com>
2021-07-18 10:02:43 -0400

    (@actions/github) add repo context

--- index.js
+++ index.js
@@ -1,25 +1,11 @@
 const github = require('@actions/github');
+const core = require('@actions/core');
 const stats = require('webpack-stats-duplicates');
 var tbl = require('./helpers/tableGenerator.js');
 
-const octokit = github.getOctokit(``);
-
-const statsLoad = async () => {
-    return new Promise((resolve, reject) => {
-        let duplicates; 
-        stats.loadConfig('./stats.json', (error, json) => {
-        if (error) {
-          console.log('Error');
-          return;
-        }
-        duplicates = stats.findDuplicates(json, {
-          whitelist: [ ]
-        });
-      });
-        resolve(duplicates);
-    });
-  
-  };
+const myToken = core.getInput('myToken');
+const octokit = github.getOctokit(myToken);
+const context = github.context;
 
 myAsyncMethod();
 
@@ -28,16 +14,30 @@ async function myAsyncMethod () {
         const dups = await statsLoad();
         var htmlTable = tbl.makeTable(dups);
         await octokit.rest.issues.createComment({
-            owner: 'georgebatalinski',
-            repo: 'questions',
-            issue_number: 2,
+            ...context.repo,
             body: htmlTable,
           });
     } catch(ex) {
         console.log( 'Error', ex );
     }
-}
+};
 
+const statsLoad = async () => {
+  return new Promise((resolve, reject) => {
+      let duplicates; 
+      stats.loadConfig('./stats.json', (error, json) => {
+      if (error) {
+        console.log('Error');
+        return;
+      }
+      duplicates = stats.findDuplicates(json, {
+        whitelist: [ ]
+      });
+    });
+      resolve(duplicates);
+  });
+
+};
 
 
 
